651bbb9d0f9946cc7b5c1b161b1f4e871b9dd1d1
Merritt Lawrenson <flamingveggies@users.noreply.github.com>
2015-04-01 23:13:00 -0700

    smarter search
    can handle some specific errors, no longer fails silently, displays list
    of results instead of failing

--- js/app.js
+++ js/app.js
@@ -2,11 +2,26 @@ $(document).ready(function() {
   var wuurl
   var wuoptions
   function wucallback(data) {
-    var weatherHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3>';
-    weatherHTML += '<p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p>';
-    weatherHTML += '<img src="'+data.current_observation.icon_url+'">';
-    $('#weather_results').html(weatherHTML);
-    $('#weather_results').show();
+    if (data.current_observation != undefined) {
+      var weatherHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3>';
+      weatherHTML += '<p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p>';
+      weatherHTML += '<img src="'+data.current_observation.icon_url+'">';
+      $('#weather_results').html(weatherHTML);
+      $('#weather_results').show();
+    } else if (data.response.results != undefined) {
+      var resultsHTML = '<h3>Multiple Results Found:</h3>';
+      $.each(data.response.results, function (i, results) {
+        resultsHTML += '<p>'+results.city+', '+(results.country_name != "USA" ? results.country_name : results.state)+'</p>';
+      });
+      $('#weather_results').html(resultsHTML);
+      $('#weather_results').show();
+    } else if (data.response.error != undefined) {
+      $('#weather_results').html('<p>Error: '+data.response.error.description+'</p>');
+      $('#weather_results').show();
+    } else {
+      $('#weather_results').html('<p>Unknown error. Please try again.</p>');
+      $('#weather_results').show();
+    }
   }
 
   $('form').submit(function (evt) {
