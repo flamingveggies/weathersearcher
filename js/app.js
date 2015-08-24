2f4fa6c308b07ff93fc506414c7af3ed6f623cf9
Merritt Lawrenson <contact@merrittlawrenson.com>
2015-08-23 21:07:37 -0700

    started on forecast section

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
       var weatherHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3>';
       weatherHTML += '<p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p>';
       weatherHTML += '<img src="'+data.current_observation.icon_url+'">';
+      $.each(data.txt_forecast.forecastday, function (i, forecast) {
+        resultsHTML += '<p id="'+results.l+'"><a href="#">'+results.city+', '+(results.country_name != "USA" ? results.country_name : results.state)+'</a></p>';
+      });
       $('#weather_results').html(weatherHTML);
       $('#weather_results').show();
     } else if (data.response.results != undefined) { // If multiple locations found, display list
@@ -17,7 +20,7 @@ $(document).ready(function() {
       $('#weather_results a').click(function (evt) { // Query weather for location results when one is selected
         evt.preventDefault();
         var searchID = $(this).parent().attr("id");
-        var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions"+searchID+".json";
+        var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day"+searchID+".json";
         $.getJSON(wuurl, wucallback);
       });
     } else if (data.response.error != undefined) { // If an error is returned
@@ -35,14 +38,14 @@ $(document).ready(function() {
     if (searchLocation === "") { // Don't actually request if no location is entered
       return;
     } else { // Otherwise you're good to go
-      var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/q/"+searchLocation+".json";
+      var urlConditionsForecast10day = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day/q/"+searchLocation+".json";
       $.getJSON(wuurl, wucallback)
     }
   });
 
   $('#current_location').click(function (evt) { // AJAX request for current location
     evt.preventDefault();
-    var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/q/autoip.json";
+    var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day/q/autoip.json";
     $.getJSON(wuurl, wucallback);
   });
   
