3c3342e2836533af40cc1e7e6e189cfbaacb8d1b
Merritt Lawrenson <contact@merrittlawrenson.com>
2015-08-23 21:26:01 -0700

    Sections sorted out and forecasts retrieved.

--- js/app.js
+++ js/app.js
@@ -2,33 +2,41 @@ $(document).ready(function() {
 
   function wucallback(data) { // Callback for AJAX
     if (data.current_observation !== undefined) { // If location found and weather retrived
-      var weatherHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3>';
-      weatherHTML += '<p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p>';
-      weatherHTML += '<img src="'+data.current_observation.icon_url+'">';
-      $.each(data.txt_forecast.forecastday, function (i, forecast) {
-        resultsHTML += '<p id="'+results.l+'"><a href="#">'+results.city+', '+(results.country_name != "USA" ? results.country_name : results.state)+'</a></p>';
+      var conditionsHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3><p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p><img src="'+data.current_observation.icon_url+'">';
+      var forecastHTML = '<h3>Forecast for '+data.current_observation.display_location.full+':</h3>';
+      $.each(data.forecast.txt_forecast.forecastday, function (i, forecast) {
+        forecastHTML += '<p>'+forecast.title+'</p>'
       });
-      $('#weather_results').html(weatherHTML);
-      $('#weather_results').show();
+      $('#results').hide();
+      $('#conditions').html(conditionsHTML);
+      $('#forecast').html(forecastHTML);
+      $('#conditions').show();
+      $('#forecast').show();
     } else if (data.response.results !== undefined) { // If multiple locations found, display list
       var resultsHTML = '<h3>Multiple Results Found:</h3>';
       $.each(data.response.results, function (i, results) {
         resultsHTML += '<p id="'+results.l+'"><a href="#">'+results.city+', '+(results.country_name != "USA" ? results.country_name : results.state)+'</a></p>';
       });
-      $('#weather_results').html(resultsHTML);
-      $('#weather_results').show();
-      $('#weather_results a').click(function (evt) { // Query weather for location results when one is selected
+      $('#results').html(resultsHTML);
+      $('#conditions').hide();
+      $('#forecast').hide();
+      $('#results').show();
+      $('#results a').click(function (evt) { // Query weather for location results when one is selected
         evt.preventDefault();
         var searchID = $(this).parent().attr("id");
-        var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day"+searchID+".json";
-        $.getJSON(wuurl, wucallback);
+        var urlConditionsForecast10day = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day"+searchID+".json";
+        $.getJSON(urlConditionsForecast10day, wucallback);
       });
     } else if (data.response.error !== undefined) { // If an error is returned
-      $('#weather_results').html('<p>Error: '+data.response.error.description+'</p>');
-      $('#weather_results').show();
+      $('#results').html('<p>Error: '+data.response.error.description+'</p>');
+      $('#conditions').hide();
+      $('#forecast').hide();
+      $('#results').show();
     } else { // If all else fails
-      $('#weather_results').html('<p>Unknown error. Please try again.</p>');
-      $('#weather_results').show();
+      $('#results').html('<p>Unknown error. Please try again.</p>');
+      $('#conditions').hide();
+      $('#forecast').hide();
+      $('#results').show();
     }
   }
 
@@ -39,20 +47,22 @@ $(document).ready(function() {
       return;
     } else { // Otherwise you're good to go
       var urlConditionsForecast10day = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day/q/"+searchLocation+".json";
-      $.getJSON(wuurl, wucallback)
+      $.getJSON(urlConditionsForecast10day, wucallback)
     }
   });
 
   $('#current_location').click(function (evt) { // AJAX request for current location
     evt.preventDefault();
-    var wuurl = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day/q/autoip.json";
-    $.getJSON(wuurl, wucallback);
+    var urlConditionsForecast10day = "http://api.wunderground.com/api/e0bb37aff4e256e4/conditions/forecast10day/q/autoip.json";
+    $.getJSON(urlConditionsForecast10day, wucallback);
   });
   
   $(document).ajaxError(function() { // Display error if weather JSON could not be retrieved
     var errorHTML = '<p>Weather data could not be found. Check your connection and try again.</p>';
-    $('#weather_results').html(errorHTML);
-    $('#weather_results').show();
+    $('#results').html(errorHTML);
+    $('#conditions').hide();
+    $('#forecast').hide();
+    $('#results').show();
   });
 
 });
