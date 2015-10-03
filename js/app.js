ed0ea0bc6b75f2034a049bf7c17f1415a0b08d4d
Merritt Lawrenson <contact@merrittlawrenson.com>
2015-10-03 19:08:29 +0000

    It has a working panel-ey interface now! It's even slightly polished looking!

--- js/app.js
+++ js/app.js
@@ -11,27 +11,27 @@ $("").click(function (evt) {
 
 function wucallback(data) { // Callback for AJAX
   if (data.current_observation !== undefined) { // If location found and weather retrived
-    var conditionsHTML = '<h3>Currently at '+data.current_observation.display_location.full+':</h3><p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p><img src="'+data.current_observation.icon_url+'">';
-    var forecastHTML = '<h3>Forecast for '+data.current_observation.display_location.full+':</h3>';
+    var conditionsHTML = '<h3>'+data.current_observation.display_location.full+'</h3><p>'+data.current_observation.weather+' &mdash; '+data.current_observation.temp_f+'&deg;F</p><img src="'+data.current_observation.icon_url+'">';
+    var forecastHTML = '<h3>'+data.current_observation.display_location.full+'</h3>';
     $.each(data.forecast.simpleforecast.forecastday, function (i, forecast) {
-      forecastHTML += '<p>'+forecast.date.weekday+'</p><img src="'+forecast.icon_url+'" alt="'+forecast.icon+'"><p>'+forecast.high.fahrenheit+'&deg;F '+forecast.low.fahrenheit+'&deg;F</p>'
+      forecastHTML += '<p>'+forecast.date.weekday+'</p><img src="'+forecast.icon_url+'" alt="'+forecast.icon+'"><p>'+forecast.high.fahrenheit+'&deg;F '+forecast.low.fahrenheit+'&deg;F</p>';
     });
     currentLocationHTML = '<li><img src="' + data.current_observation.icon_url + '">' + data.current_observation.display_location.full + ' ' + data.current_observation.temp_f +  '&deg;F<button class="savelocation">Save</button></li>';
-    $('#results').hide();
+    $('#results-wrapper').hide();
     $('#conditions').html(conditionsHTML);
     $('#forecast').html(forecastHTML);
     $('#currentlocation').html(currentLocationHTML);
-    $('#conditions').show();
-    $('#forecast').show();
+    $('#conditions-wrapper').show();
+    $('#forecast-wrapper').show();
   } else if (data.response.results !== undefined) { // If multiple locations found, display list
-    var resultsHTML = '<h3>Multiple Results Found:</h3>';
+    var resultsHTML = "";
     $.each(data.response.results, function (i, results) {
       resultsHTML += '<p id="'+results.l+'"><a href="#">'+results.city+', '+(results.country_name != "USA" ? results.country_name : results.state)+'</a></p>';
     });
     $('#results').html(resultsHTML);
-    $('#conditions').hide();
-    $('#forecast').hide();
-    $('#results').show();
+    $('#conditions-wrapper').hide();
+    $('#forecast-wrapper').hide();
+    $('#results-wrapper').show();
     $('#results a').click(function (evt) { // Query weather for location results when one is selected
       evt.preventDefault();
       var searchID = $(this).parent().attr("id");
@@ -40,14 +40,14 @@ function wucallback(data) { // Callback for AJAX
     });
   } else if (data.response.error !== undefined) { // If an error is returned
     $('#results').html('<p>Error: '+data.response.error.description+'</p>');
-    $('#conditions').hide();
-    $('#forecast').hide();
-    $('#results').show();
+    $('#conditions-wrapper').hide();
+    $('#forecast-wrapper').hide();
+    $('#results-wrapper').show();
   } else { // If all else fails
     $('#results').html('<p>Unknown error. Please try again.</p>');
-    $('#conditions').hide();
-    $('#forecast').hide();
-    $('#results').show();
+    $('#conditions-wrapper').hide();
+    $('#forecast-wrapper').hide();
+    $('#results-wrapper').show();
   }
 }
 
@@ -71,7 +71,7 @@ $('#current_location').click(function (evt) { // AJAX request for current locati
 $(document).ajaxError(function() { // Display error if weather JSON could not be retrieved
   var errorHTML = '<p>Weather data could not be found. Check your connection and try again.</p>';
   $('#results').html(errorHTML);
-  $('#conditions').hide();
-  $('#forecast').hide();
-  $('#results').show();
+  $('#conditions-wrapper').hide();
+  $('#forecast-wrapper').hide();
+  $('#results-wrapper').show();
 });
